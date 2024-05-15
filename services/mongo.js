7935c77b83308f48d952845e8fd6eaba0dd8806e
code-X-AJ <amit24032005@gmail.com>
2024-05-15 11:37:01 +0530

    Add: Cloud database added

--- services/mongo.js
+++ services/mongo.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
+const mongoURI = process.env.MONGO_URI;
 
-function connectDB(){
+async function connectDB(){
     try {
-        mongoose.connect("mongodb://0.0.0.0:27017/",{
+        await mongoose.connect(mongoURI ,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
